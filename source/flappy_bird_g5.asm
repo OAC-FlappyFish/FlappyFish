ca60b32ed850b527511f2ac1bb255400540f4f31
jonibeatles <jonibeatles@gmail.com>
2014-06-11 23:06:15 -0300

    Muitas coisas nesse comit, desenho do flappy, fisica e coisas basicas do loop principal.
    Signed-off-by: jonibeatles <jonibeatles@gmail.com>

--- source/flappy_bird_g5.asm
+++ source/flappy_bird_g5.asm
@@ -9,17 +9,27 @@
 .data
 	# VARIAVEIS DE GERENCIAMENTO DO DISPLAY
 	bitmap_address: .word 0x10040000
-	bitmap_width:   .word 512
-	bitmap_height:  .word 512
+	bitmap_width:   .word 128
+	bitmap_height:  .word 128
+	
+	nl:	.asciiz "\n"
 
 	
 	#variaveis do jogo
 	
 	#PLAYER
-	P_posY:		.word		100			#afetada pela gravidade
-	P_posX:		.word		70			#nunca mais sera mudada
-	P_velY:		.word		0			#inicialmente parado até receber input
-
+	P_posY:		.word 45			#afetada pela gravidade
+	P_posX:		.word 32			#nunca mais sera mudada
+	P_velY:		.word 0			#inicialmente parado até receber input
+	
+	P_aceleracao:	.word	2			#aceleracao da gravidade, sempre constante
+	
+	P_force:	.word	-20			#força aplicada qnd receber o input
+	
+	frameCounter:	.word 1				#contar em qual frame está
+	
+	frequenciaForce:	.word	6		#aplicar força a cada 100 frames(teste apenas)
+	
 	#cores
 	
 	
@@ -50,50 +60,233 @@ main:
 		
 		jal clearScreen
 		
-		#draw flappy
 		lw $a0,P_posX
 		lw $a1,P_posY
 		
+		#draw flappy
+		
 		jal drawFlappy
 	
+		#jal drawQuadrado
 	
 	
-	
-	#loop_principal:
-	
+	loop_principal:
 	
 	
+		jal atualizarVelocidade
+		jal atualizarPosicoes
 	
 	
+		jal clearScreen		#resetar o display
+		
+		
+		lw $a0,P_posX
+		lw $a1,P_posY
+		#draw flappy
+		jal drawFlappy
+		
+		lw $t7,frameCounter			#registrador nao guardado na pilha!!!
+		bge $t7,18,naoChamaForce
+		jal applyForce
+		naoChamaForce:
+	
+		jal incFrame
 	
 	
-	
-	
-	
-	
-	#j loop_principal
+	j loop_principal
 	
 	
 	j exit
 	
 	#procedures
 	
-	#a0 == cor ||  a1 == x  || a2 == y
-	drawPixel:
-		lw $t0,bitmap_width
-		mul $t1,$t0,$a2		#obter endereço
-		add $t2,$t1,$a1
-		sll $t2,$t2,2		#multyply por 4
-		lw $t3,bitmap_address
-		add $t4,$t3,$t2
-		sw $a0,($t4)
+	
+	applyForce:
+		addi $sp,$sp,-16
+		sw $s0,($sp)
+		sw $s1,4($sp)
+		sw $s2,8($sp)
+		sw $s3,12($sp)
+		
+		
+		lw $s3,frameCounter
+		lw $s2,frequenciaForce
+		div $s3,$s2
+		
+		mfhi $s1
+		
+		bne $s1,0,naoAplicar
+		
+		lw $s0,P_velY
+		lw $s1,P_force
+		
+		add $s2,$s0,$s1		#s2(nova velocidade)  =  velocidadeAntiga + forca
+		
+		sw $s2,P_velY
+		
+		
+		
+		naoAplicar:
+		
+		lw $s0,($sp)
+		lw $s1,4($sp)
+		lw $s2,8($sp)
+		lw $s3,12($sp)
+		addi $sp,$sp,16
+		
+		
 		
 		jr $ra
 	
+	
+	incFrame:
+		addi $sp,$sp,-4
+		sw $s0,0($sp)
+		
+		lw $s0,frameCounter
+		
+		addi $s0,$s0,1
+		
+		sw $s0,frameCounter
+		
+		
+		
+		lw $s0,0($sp)
+		addi $sp,$sp,4
+		
+		jr $ra
+	
+	atualizarPosicoes:
+		addi $sp,$sp,-16
+		sw $s0,($sp)
+		sw $s1,4($sp)
+		sw $s2,8($sp)
+		sw $s3,12($sp)
+		
+		lw $s0,P_posY
+		lw $s1,P_velY
+		
+		add $s2,$s0,$s1		#nova posicao = posicaoantiga + velocidade atual
+		
+		sw $s2,P_posY
+		
+		lw $s0,($sp)
+		lw $s1,4($sp)
+		lw $s2,8($sp)
+		lw $s3,12($sp)
+		addi $sp,$sp,16
+		
+		jr $ra
+	
+	atualizarVelocidade:
+		addi $sp,$sp,-16
+		sw $s0,($sp)
+		sw $s1,4($sp)
+		sw $s2,8($sp)
+		sw $s3,12($sp)
+		
+		lw $s0,P_aceleracao
+		lw $s1,P_velY
+		
+		add $s2,$s0,$s1		#s2 = velocidade + aceleracao (nova velocidade)
+		
+		sw $s2,P_velY
+		
+		
+		
+		lw $s0,($sp)
+		lw $s1,4($sp)
+		lw $s2,8($sp)
+		lw $s3,12($sp)
+		addi $sp,$sp,16
+		
+		jr $ra
+	
+	#a0 == cor ||  a1 == x  || a2 == y
+	drawPixel:
+		addi $sp,$sp,-36
+		sw $ra,0($sp)
+		sw $a0,4($sp)
+		sw $a1,8($sp)
+		sw $a2,12($sp)
+		sw $t0,16($sp)
+		sw $t1,20($sp)
+		sw $t2,24($sp)
+		sw $t3,28($sp)
+		sw $t4,32($sp)
+		
+		
+		lw $t0,bitmap_width
+		mul $t1,$t0,$a2		#t1 = width * y
+		add $t2,$t1,$a1		#t2 = (width * y) + x
+		sll $t2,$t2,2		#t2 = 4 * ((width * y) + x)
+		lw $t3,bitmap_address
+		add $t4,$t3,$t2		#t4 = address + 4 * ((width * y) + x)
+		sw $a0,0($t4)
+		
+		lw $ra,0($sp)
+		lw $a0,4($sp)
+		lw $a1,8($sp)
+		lw $a2,12($sp)
+		lw $t0,16($sp)
+		lw $t1,20($sp)
+		lw $t2,24($sp)
+		lw $t3,28($sp)
+		lw $t4,32($sp)
+		addi $sp,$sp,36
+		
+		jr $ra
+	
+	
+	#a0 == x ; a1 == y
+	drawQuadrado:
+		addi $sp,$sp,-12
+		sw $ra,($sp)
+		sw $a0,4($sp)
+		sw $a1,8($sp)
+		
+		move $t0,$a0
+		move $t1,$a1
+		
+		#pixels
+		addi $a1,$t0,0
+		addi $a2,$t1,0
+		
+		jal drawPixel
+		
+			
+		#pixels
+		addi $a1,$t0,0
+		addi $a2,$t1,1
+		jal drawPixel
+		
+			
+						
+		#pixels
+		addi $a1,$t0,1
+		addi $a2,$t1,0
+		jal drawPixel
+			
+		#pixels
+		addi $a1,$t0,1
+		addi $a2,$t1,1
+		jal drawPixel
+		
+				
+		lw $ra,($sp)
+		lw $a0,4($sp)
+		lw $a1,8($sp)
+		addi $sp,$sp,12
+		
+		jr $ra
+	
+	
 	#a0 == x ; a1 == y
 	drawFlappy:
-		addi $sp,$sp,-4
+		addi $sp,$sp,-12
 		sw $ra,($sp)
+		sw $a0,4($sp)
+		sw $a1,8($sp)
 		
 		move $t0,$a0
 		move $t1,$a1
@@ -306,9 +499,328 @@ main:
 			addi $a2,$t1,1
 			jal drawPixel
 			
+			#pixels
+			addi $a1,$t0,14
+			addi $a2,$t1,3
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,14
+			addi $a2,$t1,4
+			jal drawPixel
+			
+			##############
+			#barbatanas pretas
+			
+			#pixels
+			addi $a1,$t0,8
+			addi $a2,$t1,6
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,8
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,8
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,3
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,4
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,5
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,6
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,8
+			jal drawPixel
+			
+		
+		drawPixelsBoca:
+			lw $a0,boca
+			
+			
+			#pixels
+			addi $a1,$t0,14
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,15
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,16
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,14
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,15
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,16
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			
+			###### fim boca ######
+			
+		drawPixelsAmareloClaro:
+			lw $a0,amarelo_claro
+			
+			#pixels
+			addi $a1,$t0,1
+			addi $a2,$t1,2
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,2
+			addi $a2,$t1,1
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,3
+			addi $a2,$t1,3
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,4
+			addi $a2,$t1,4
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,5
+			addi $a2,$t1,3
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,6
+			addi $a2,$t1,2
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,7
+			addi $a2,$t1,2
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,8
+			addi $a2,$t1,1
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,9
+			addi $a2,$t1,1
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,10
+			addi $a2,$t1,1
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,1
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,12
+			addi $a2,$t1,1
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,17
+			addi $a2,$t1,4
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,17
+			addi $a2,$t1,5
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,17
+			addi $a2,$t1,6
+			jal drawPixel
+			
+		drawPixelsAmareloEscuro:
+			lw $a0,amarelo_escuro
+			
+			#pixels
+			addi $a1,$t0,1
+			addi $a2,$t1,6
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,1
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,1
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,2
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,3
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,4
+			addi $a2,$t1,6
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,5
+			addi $a2,$t1,7
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,6
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,7
+			addi $a2,$t1,8
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,8
+			addi $a2,$t1,9
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,9
+			addi $a2,$t1,9
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,10
+			addi $a2,$t1,9
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,11
+			addi $a2,$t1,9
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,12
+			addi $a2,$t1,9
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,13
+			addi $a2,$t1,9
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,10
+			addi $a2,$t1,4
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,10
+			addi $a2,$t1,5
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,10
+			addi $a2,$t1,6
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,10
+			addi $a2,$t1,7
+			jal drawPixel
+			
+		
+		drawPixelsOlho:
+			lw $a0,white
+			
+			#pixels
+			addi $a1,$t0,13
+			addi $a2,$t1,2
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,13
+			addi $a2,$t1,3
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,13
+			addi $a2,$t1,4
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,13
+			addi $a2,$t1,5
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,14
+			addi $a2,$t1,5
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,15
+			addi $a2,$t1,4
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,15
+			addi $a2,$t1,3
+			jal drawPixel
+			
+			#pixels
+			addi $a1,$t0,14
+			addi $a2,$t1,2
+			jal drawPixel
+			
+			
 			
 		lw $ra,($sp)
-		addi $sp,$sp,4
+		lw $a0,4($sp)
+		lw $a1,8($sp)
+		addi $sp,$sp,12
 		
 		jr $ra
 	
