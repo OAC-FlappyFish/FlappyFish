726a56cb02c023962b623d6b58ce23cf329a34dc
jonibeatles <jonibeatles@gmail.com>
2014-06-16 01:50:28 -0300

    Lógica dos spawns terminada.
    Signed-off-by: jonibeatles <jonibeatles@gmail.com>

--- src/flappy_bird_g5.s
+++ src/flappy_bird_g5.s
@@ -46,11 +46,18 @@
 	obstaculosWidth:	.word	15
 	obstaculosCor:		.word	0x009933
 	
-	obstaculo1:		.word	1,0,40		#presente	||	tipo	||	pos_x
-	obstaculo2:		.word	1,4,90		#presente	||	tipo	||	pos_x
+	obstaculo1:		.word	0,0,0		#presente	||	tipo	||	pos_x
+	obstaculo2:		.word	0,0,0		#presente	||	tipo	||	pos_x
 	
 	obstaculoInativo:	.word	1
 	deveFazerSpawn:		.word	1
+	
+	gapHeight:		.word	26
+	
+	posUltimoSpawn:		.word	0
+	
+	intervaloEntreSpawns:	.word	40
+	
 	#cores
 	
 	
@@ -102,16 +109,21 @@ main:
 		lw $s0,P_posY
 		jal drawRetangulo
 		
-		jal clearObstaculos
+		li $a0,0
+		jal drawObstaculos
 		
 	
 		jal atualizarVelocidade
 		jal atualizarPosicoes
 		jal atualizarObstaculos
+		jal atualizarFlagsSpawn
 		
 	
 		#jal clearScreen		#resetar o display
 		
+		#spawns
+		jal destruirSpawns
+		jal fazerSpawns
 		
 		
 		#draws
@@ -119,6 +131,8 @@ main:
 		lw $a1,P_posY
 		jal drawFlappy
 		
+		
+		li $a0,1
 		jal drawObstaculos
 		
 		lw $t7,frameCounter			#registrador nao guardado na pilha!!!
@@ -137,6 +151,10 @@ main:
 		jal wait
 		jal wait
 		jal wait
+		jal wait
+		jal wait
+		jal wait
+		jal wait
 	
 	
 	j loop_principal
@@ -146,10 +164,9 @@ main:
 	
 	#procedures
 	
-	
-	clearObstaculos:
-		addi $sp,$sp,-40
-		sw $s0,0($sp)
+	fazerSpawns:
+		addi $sp,$sp,-36
+		sw $s0,($sp)
 		sw $s1,4($sp)
 		sw $s2,8($sp)
 		sw $s3,12($sp)
@@ -157,100 +174,56 @@ main:
 		sw $t1,20($sp)
 		sw $t2,24($sp)
 		sw $t3,28($sp)
-		sw $t4,32($sp)
-		sw $ra,36($sp)
+		sw $ra,32($sp)
+	
+		lw $s0,frameCounter
+		lw $s1,intervaloEntreSpawns
 		
-		la $s1,obstaculo1	#s1 == obs1
-		la $s2,obstaculo2	#s2 == obs2
+		div $s0,$s1
 		
-		# presente	||	tipo	||	pos_x
-		#verificar atividade
+		mfhi $t0	#t0 == resto
 		
-		#desenho do 1
+		beq $zero,$t0,podeFazerSpawn
+		j naoFaz
 		
-		la $t3,tiposDeObstaculos
-		la $t4,alturaDoTipo
+		podeFazerSpawn:
+		lw $s2,deveFazerSpawn
+		li $t1,1
+		beq $t1,$s2,fazDo1
+		li $t1,2
+		beq $t1,$s2,fazDo2
+		j naoFaz
 		
-		lw $s0,0($s1)		#s0 == obs1 presente
+		fazDo1:
+		la $s1,obstaculo1
 		
-		beq $s0,$zero,naoDesenha1_2
+		li $t0,1		#presente
+		li $t1,0		#tipo
+		lw $t2,bitmap_width	#posicao x
 		
-		#desenha1
-		lw $t0,4($s1)	#tipo em t0
-		sll $t0,$t0,2	#mult o tipo por 4 -> endereçamento
+		sw $t0,0($s1)
+		sw $t1,4($s1)
+		sw $t2,8($s1)
 		
-		add $t0,$t4,$t0		#end final
+		j naoFaz
 		
-		lw $t0,($t0)		#t0 recebe a altura do obstaculo
+		fazDo2:
 		
-		lw $t1,obstaculosWidth
-		lw $t2,azul_fundo
-		lw $t3,8($s1)		#t3 == posX
+		la $s1,obstaculo2
 		
-		lw $t4,bitmap_height
+		li $t0,1		#presente
+		li $t1,4		#tipo
+		lw $t2,bitmap_width	#posicao x
 		
-		sub $t4,$t4,$t0		#t4 recebe posicao y
+		sw $t0,0($s1)
+		sw $t1,4($s1)
+		sw $t2,8($s1)
 		
-		#arrumar argumentos para o desenho do obstaculo
-		move $a0,$t2
-		move $a1,$t1
-		move $a2,$t0
-		move $a3,$t3
-		move $s0,$t4
+		j naoFaz
 		
-		jal drawRetangulo
+		naoFaz:
 		
-		
-		
-		
-		naoDesenha1_2:
-		
-		
-		
-		#desenho do 2
-		
-		la $t3,tiposDeObstaculos
-		la $t4,alturaDoTipo
-		
-		lw $s0,0($s2)		#s0 == obs2 presente
-		
-		beq $s0,$zero,naoDesenha2_2
-		
-		
-		#desenha2
-		lw $t0,4($s2)	#tipo em t0
-		sll $t0,$t0,2	#mult o tipo por 4 -> endereçamento
-		
-		add $t0,$t4,$t0		#end final
-		
-		lw $t0,($t0)		#t0 recebe a altura do obstaculo
-		
-		lw $t1,obstaculosWidth
-		lw $t2,azul_fundo
-		lw $t3,8($s2)		#t3 == posX
-		
-		lw $t4,bitmap_height
-		
-		sub $t4,$t4,$t0		#t4 recebe posicao y
-		
-		#arrumar argumentos para o desenho do obstaculo
-		move $a0,$t2
-		move $a1,$t1
-		move $a2,$t0
-		move $a3,$t3
-		move $s0,$t4
-		
-		jal drawRetangulo
-		
-		
-		
-		
-		
-		naoDesenha2_2:
-		
-		
-		
-		lw $s0,0($sp)
+		lw $s0,($sp)
 		lw $s1,4($sp)
 		lw $s2,8($sp)
 		lw $s3,12($sp)
@@ -258,9 +231,148 @@ main:
 		lw $t1,20($sp)
 		lw $t2,24($sp)
 		lw $t3,28($sp)
-		lw $t4,32($sp)
-		lw $ra,36($sp)
-		addi $sp,$sp,40
+		lw $ra,32($sp)
+		addi $sp,$sp,36
+		
+		
+		
+		jr $ra
+		
+		
+	destruirSpawns:
+		addi $sp,$sp,-32
+		sw $s0,($sp)
+		sw $s1,4($sp)
+		sw $s2,8($sp)
+		sw $s3,12($sp)
+		sw $t0,16($sp)
+		sw $t1,20($sp)
+		sw $t2,24($sp)
+		sw $t3,28($sp)
+		
+		
+		la $s1,obstaculo1
+		la $s2,obstaculo2
+		
+		lw $t0,8($s1)		#posicao do 1
+		lw $t1,8($s2)		#posicao do 2
+		
+		lw $t2,obstaculosWidth
+		sub $t2,$zero,$t2
+		
+		ble $t0,$t2,destruir1
+		j sair1
+		
+		
+		destruir1:
+		sw $zero,0($s1)
+		
+		sair1:
+		
+		
+		#para o 2
+		
+		ble $t1,$t2,destruir2
+		j sair2
+		
+		
+		destruir2:
+		sw $zero,0($s2)
+		
+		sair2:
+		
+		lw $s0,($sp)
+		lw $s1,4($sp)
+		lw $s2,8($sp)
+		lw $s3,12($sp)
+		lw $t0,16($sp)
+		lw $t1,20($sp)
+		lw $t2,24($sp)
+		lw $t3,28($sp)
+		addi $sp,$sp,32
+		
+		
+		
+		jr $ra
+	
+	
+	atualizarFlagsSpawn:
+		addi $sp,$sp,-32
+		sw $s0,($sp)
+		sw $s1,4($sp)
+		sw $s2,8($sp)
+		sw $s3,12($sp)
+		sw $t0,16($sp)
+		sw $t1,20($sp)
+		sw $t2,24($sp)
+		sw $t3,28($sp)
+		
+		lw $s0,deveFazerSpawn
+		lw $s3,obstaculoInativo
+		la $s1,obstaculo1
+		la $s2,obstaculo2
+		
+		lw $t0,8($s1)		#posicao do 1
+		lw $t1,8($s2)		#posicao do 2
+		
+		ble $t0,$t1,ultimo1
+		j ultimo2
+		
+		ultimo1:
+		
+		sw $t0,posUltimoSpawn
+		j saiDaqui
+		
+		ultimo2:
+		
+		sw $t1,posUltimoSpawn
+		j saiDaqui
+		
+		
+		saiDaqui:
+		
+		lw $t0,0($s1)		#present 1
+		lw $t1,0($s2)		#presente 2
+		
+		beq $t0,$zero,naoPresente1
+		beq $t1,$zero,naoPresente2
+		
+		j presentes
+		
+		
+		
+		naoPresente1:
+			li $t2,1
+			sw $t2,obstaculoInativo
+			sw $t2,deveFazerSpawn
+			j sai
+			
+		naoPresente2:
+			li $t2,1
+			sw $t2,obstaculoInativo
+			li $t2,2
+			sw $t2,deveFazerSpawn
+			j sai
+			
+		
+		presentes:
+			sw $zero,obstaculoInativo
+			sw $zero,deveFazerSpawn
+			j sai
+		
+		sai:
+		
+		lw $s0,($sp)
+		lw $s1,4($sp)
+		lw $s2,8($sp)
+		lw $s3,12($sp)
+		lw $t0,16($sp)
+		lw $t1,20($sp)
+		lw $t2,24($sp)
+		lw $t3,28($sp)
+		addi $sp,$sp,32
+		
+		
 		
 		jr $ra
 	
@@ -309,8 +421,9 @@ main:
 		jr $ra
 		
 	
+	#recebe a0:		a0 == 0 indica clear	||| 	a0 == 1 indica draw
 	drawObstaculos:
-		addi $sp,$sp,-40
+		addi $sp,$sp,-44
 		sw $s0,0($sp)
 		sw $s1,4($sp)
 		sw $s2,8($sp)
@@ -321,6 +434,7 @@ main:
 		sw $t3,28($sp)
 		sw $t4,32($sp)
 		sw $ra,36($sp)
+		sw $a0,40($sp)
 		
 		la $s1,obstaculo1	#s1 == obs1
 		la $s2,obstaculo2	#s2 == obs2
@@ -346,7 +460,18 @@ main:
 		lw $t0,($t0)		#t0 recebe a altura do obstaculo
 		
 		lw $t1,obstaculosWidth
+		
+		beq $a0,$zero,setarClear1
+		
 		lw $t2,obstaculosCor
+		j saiClear1
+		
+		setarClear1:
+		lw $t2,azul_fundo
+		
+		saiClear1:
+		
+		
 		lw $t3,8($s1)		#t3 == posX
 		
 		lw $t4,bitmap_height
@@ -354,16 +479,49 @@ main:
 		sub $t4,$t4,$t0		#t4 recebe posicao y
 		
 		#arrumar argumentos para o desenho do obstaculo
-		move $a0,$t2
-		move $a1,$t1
-		move $a2,$t0
-		move $a3,$t3
-		move $s0,$t4
+		move $a0,$t2		#cor
+		move $a1,$t1		#width
+		move $a2,$t0		#altura
+		move $a3,$t3		#pos x
+		move $s0,$t4		#pos y
 		
+		lw $t0,bitmap_width
+		add $t1,$a3,$a1
+		
+		bgt $t1,$t0,ajustarMaior
+		blt $a3,$zero,ajustar
+		j draw
+		
+		ajustar:
+		add $a1,$a1,$a3		# pos + width
+		li $a3,0
+		j draw
+		
+		ajustarMaior:
+		sub $t1,$t1,$t0
+		sub $a1,$a1,$t1		#tirar tudo que sobrou para fora da tela
+		j draw
+		
+		draw:
 		jal drawRetangulo
 		
+		#alterar argumentos para o complemento do cano
 		
 		
+		#set altura
+		lw $t0,bitmap_height
+		
+		sub $t0,$t0,$a2
+		
+		lw $a2,gapHeight
+		
+		sub $a2,$t0,$a2
+		###############
+		
+		#set pos y
+		li $s0,0
+		
+		jal drawRetangulo
 		
 		naoDesenha1:
 		
@@ -388,7 +546,20 @@ main:
 		lw $t0,($t0)		#t0 recebe a altura do obstaculo
 		
 		lw $t1,obstaculosWidth
+		
+		lw $a0,40($sp)
+		
+		beq $a0,$zero,setarClear2
+		
 		lw $t2,obstaculosCor
+		j saiClear2
+		
+		setarClear2:
+		lw $t2,azul_fundo
+		
+		saiClear2:
+		
+		
 		lw $t3,8($s2)		#t3 == posX
 		
 		lw $t4,bitmap_height
@@ -401,6 +572,43 @@ main:
 		move $a2,$t0
 		move $a3,$t3
 		move $s0,$t4
+		
+		#ajustes para nao sair da tela
+		lw $t0,bitmap_width
+		add $t1,$a3,$a1
+		
+		bgt $t1,$t0,ajustarMaior2
+		blt $a3,$zero,ajustar2
+		j draw2
+		
+		ajustar2:
+		add $a1,$a1,$a3		# pos + width
+		li $a3,0
+		j draw2
+		
+		ajustarMaior2:
+		sub $t1,$t1,$t0
+		sub $a1,$a1,$t1		#tirar tudo que sobrou para fora da tela
+		j draw2
+		
+		draw2:
+		jal drawRetangulo
+		
+		#alterar argumentos para o complemento do cano
+		
+		
+		#set altura
+		lw $t0,bitmap_height
+		
+		sub $t0,$t0,$a2
+		
+		lw $a2,gapHeight
+		
+		sub $a2,$t0,$a2
+		###############
+		
+		#set pos y
+		li $s0,0
 		
 		jal drawRetangulo
 		
@@ -422,7 +630,8 @@ main:
 		lw $t3,28($sp)
 		lw $t4,32($sp)
 		lw $ra,36($sp)
-		addi $sp,$sp,40
+		lw $a0,40($sp)
+		addi $sp,$sp,44
 		
 		jr $ra
 	
